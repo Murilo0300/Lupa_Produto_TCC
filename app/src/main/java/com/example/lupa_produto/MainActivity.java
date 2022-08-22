5606cf9fcea84c641a8df570a48ed6074c5659a1
Murilo Cavalcanti <murilo0300@hotmail.com.br>
2022-08-21 23:07:51 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lupa_produto/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lupa_produto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
